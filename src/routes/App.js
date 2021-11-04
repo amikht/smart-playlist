6b0c489ae0aa5c6f92103b0673530a780cb6693a
Aaron Thompson <amt4049@rit.edu>
2021-11-04 00:15:11 -0400

    Refactors directory structure, implements basic homepage

---
+++ src/routes/App.js
@@ -0,0 +1,12 @@
+import MenuBar from "../ui-components/MenuBar";
+
+function App() {
+  return (
+    <div className="container min-w-full mx-auto App">
+      <MenuBar />
+      <h1 className="flex py-9 text-5xl justify-center">Organize your music today!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Register.js
@@ -0,0 +1,10 @@
+import MenuBar from '../ui-components/MenuBar';
+
+export default function Register(...props) {
+    return (
+        <>
+            <MenuBar/>
+            <h1 className="flex py-9 text-5xl justify-center">Let's get organized!</h1>
+        </>
+    );
+}
