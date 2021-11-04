928d8ee94bd5983e30562e84afa6271d1b19530b
Aaron Thompson <amt4049@rit.edu>
2021-11-04 00:15:51 -0400

    Refactors index.js for tailwind

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+
+import App from './routes/App';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
