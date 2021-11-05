0979632ae12cb22f2a4fb08e05b7c5dc12e148aa
Aaron Thompson <amt4049@rit.edu>
2021-11-04 22:14:48 -0400

    Updates Button.js to use better spacing

--- src/ui-components/Button.js
+++ src/ui-components/Button.js
@@ -4,7 +4,7 @@ export default function Button(props) {
     return (
     <Link 
         to={props.linkTo}
-        className={"font-bold text-white py-2 px-4 mx-2 rounded bg-" + props.buttonColor + "-400 hover:bg-" + props.buttonColor + "-600 float-right " + props.buttonStyle}>
+        className={"font-bold text-white py-2 px-4 rounded bg-" + props.buttonColor + "-400 hover:bg-" + props.buttonColor + "-600 float-right " + props.buttonStyle}>
             {props.buttonText}
     </Link>
     );
--- src/ui-components/MenuBar.js
+++ src/ui-components/MenuBar.js
@@ -4,7 +4,7 @@ export default function MenuBar(...props) {
     return (
         <div className="flex flex-row container mx-auto max-w-screen-2xl bg-gray-200 px-3 py-4">
             <Button buttonText="Smart Playlists!" buttonColor="gray" linkTo="/" buttonStyle={"flex-none"}/>
-            <div className="flex relative container max-w-7xl justify-end font-sans font-light">
+            <div className="flex relative container max-w-7xl space-x-4 justify-end font-sans font-light">
                 <Button buttonText="Login" buttonColor="blue" linkTo="/login"/>
                 <Button buttonText="Register" buttonColor="green" linkTo="/register"/>
             </div>
